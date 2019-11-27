9e96d09dfa4315600763c2a33ba4f2200319c3ef
Jeffery Ho <chiachun2491@gmail.com>
2019-11-27 14:55:35 +0800

    [Update] Finish CBC CTR Decrypt

--- hw3/DecryptAES.py
+++ hw3/DecryptAES.py
@@ -13,11 +13,23 @@ def ECB(block_array, key):
 
 
 def CBC(block_array, key):
-    return 0
+    decrypt_array = []
+    iv = b'1234567890123456'
+    cipher = AES.new(key, AES.MODE_ECB)
+    for block in block_array:
+        decrypt_array.append(byte_xor(iv, cipher.decrypt(block)))
+        iv = block
+    return decrypt_array
 
 
 def CTR(block_array, key):
-    return 0
+    decrypt_array = []
+    counter = b'\xe5\xad\xa6\xe4\xb9\xa0\xe5\xbe\x00\x00\x00\x00\x00\x00\x00\x01'
+    cipher = AES.new(key, AES.MODE_ECB)
+    for block in block_array:
+        decrypt_array.append(byte_xor(cipher.decrypt(counter), block))
+        counter = byte_add(counter, 1)
+    return decrypt_array
 
 
 def DecryptAES(block_array, key, mode):
@@ -48,7 +60,7 @@ def main():
 
     magic_number, size, maximum_value, blocks = openppm(ppm_name)
 
-    arr_aes = DecryptAES(blocks, "put key", 'ECB')
+    arr_aes = DecryptAES(blocks, "put key", 'CBC')
     # arr_aes = blocks
 
     writeppm(result_ppm, magic_number, size, maximum_value, arr_aes)
