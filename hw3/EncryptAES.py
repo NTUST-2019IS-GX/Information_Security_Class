57fa2104a2991b4ad984dfb44ca46e6f49e8df00
YuKai Lee <kevin991239@gmail.com>
2019-11-27 15:08:33 +0800

    [Update] Upload CBC & CTR for EncryptAES

--- hw3/EncryptAES.py
+++ hw3/EncryptAES.py
@@ -13,11 +13,24 @@ def ECB(block_array, key):
 
 
 def CBC(block_array, key):
-    return 0
+    encrypt_array = []
+    cipher = AES.new(key, AES.MODE_ECB)
+    cipher_block = b'1234567890123456'  # Initial Vector
+    for block in block_array:
+        cipher_block = byte_xor(cipher_block, block)
+        encrypt_array.append(cipher.encrypt(cipher_block))
+    return encrypt_array
 
 
 def CTR(block_array, key):
-    return 0
+    encrypt_array = []
+    cipher = AES.new(key, AES.MODE_ECB)
+    initial_vector = b'\xe5\xad\xa6\xe4\xb9\xa0\xe5\xbe\x00\x00\x00\x00\x00\x00\x00\x00'  # Initial Vector
+    for block in block_array:
+        byte_add(initial_vector, 1)
+        cipher_block = cipher.encrypt(initial_vector)
+        encrypt_array.append(byte_xor(cipher_block, block))
+    return encrypt_array
 
 
 def EncryptAES(block_array, key, mode):
@@ -48,7 +61,7 @@ def main():
 
     magic_number, size, maximum_value, blocks = openppm(ppm_name)
 
-    arr_aes = EncryptAES(blocks, "put key", 'ECB')
+    arr_aes = EncryptAES(blocks, "put key", 'CBC')
     # arr_aes = blocks
 
     writeppm(result_ppm, magic_number, size, maximum_value, arr_aes)
